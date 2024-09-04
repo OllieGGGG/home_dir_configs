d7f831196b8bce9d42ec79f2795a1d68e1eca704
Oleg <oleg.ignatenko12@gmail.com>
2024-09-04 20:46:03 +0300

    Updates.

--- .config/nvim/lua/bestie/init.lua
+++ .config/nvim/lua/bestie/init.lua
@@ -1,3 +1,4 @@
 require("bestie.remap")
 require("bestie.packer")
 require("bestie.set")
+require("bestie.colorscheme")
--- .config/nvim/lua/bestie/packer.lua
+++ .config/nvim/lua/bestie/packer.lua
@@ -2,7 +2,7 @@
 -- List all plugins here you need.
 
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
 return require('packer').startup(function(use)
   -- Packer can manage itself
